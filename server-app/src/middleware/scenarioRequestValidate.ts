744a8b3bf12c7b3f1bb26fe19a2b3310621a991b
Kanha Agrawal <kanha.agr11@gmail.com>
2022-08-26 16:29:23 +0530

    improve flexfile validation

--- server-app/src/middleware/scenarioRequestValidate.ts
+++ server-app/src/middleware/scenarioRequestValidate.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import log from "../logger";
-import { isUndefined } from "lodash";
+import { isArray, isNumber, isUndefined, toNumber } from "lodash";
 import { fileConfigSchema, scenarioConfigSchema } from "../schema/scenarioRequest.schema";
 
 const scenarioValidation = () => async (
@@ -9,17 +9,20 @@ const scenarioValidation = () => async (
   next: NextFunction
 ) => {
   try {
+    if (isArray(req.files.flexfile))
+      throw Error("Multiple files not allowed")
     if (isUndefined(req.files?.flexfile) && isUndefined(req.body.scenarioConfig))
       return res.status(400).send("Configuration required!")
-    else if (isUndefined(req.files)) {
+
+    if (isUndefined(req.files)) {
       await scenarioConfigSchema.validate(req.body.scenarioConfig)
       req.scenarioConfig = structuralizeScenarioRequest(null, req.body.scenarioConfig, -1)
     }
     else {
-      const fileData:any = JSON.parse(req.files.flexfile.data)
-      const scenarioIndex: number = req.body.scenarioIndex
-      if (isUndefined(scenarioIndex))
-        throw Error("scenarioIndex required")
+      const fileData: any = JSON.parse(req.files.flexfile.data)
+      const scenarioIndex: number = toNumber(req.body.scenarioIndex)
+      if (isUndefined(scenarioIndex) || isNaN(scenarioIndex) || !isNumber(scenarioIndex))
+        throw Error("scenario index invalid")
       if (scenarioIndex > fileData.scenario.length - 1)
         throw Error("index out of bound");
       await fileConfigSchema.validate(fileData)
@@ -33,8 +36,8 @@ const scenarioValidation = () => async (
 }
 
 const structuralizeScenarioRequest = (fileData: any, scenarioConfig: any, scenarioIndex: number) => {
-  let scenario:any;
-  let requests:any;
+  let scenario: any;
+  let requests: any;
   if (fileData) {
     const scenarioId = fileData.scenario[scenarioIndex]._id
     requests = fileData.requests.filter((request: any, index: number) => request.scenarioId === scenarioId)
