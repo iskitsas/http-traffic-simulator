aa870de89702772f436e674b1f695bfdcb29cfcc
iskitsas <iskitsas@gmail.com>
2023-06-04 09:13:54 +0300

    Fix issue with mac installation

--- desktop-app/Constants/constants.js
+++ desktop-app/Constants/constants.js
@@ -1,1 +1,6 @@
-module.exports = { APP_NAME:"Flexbench" }
+// In case we need to provide a different name in package.json than Flexbench, eg flexbench-desktop,
+// then in Mac OS, the installer will create a directory with name Flexbench instead of flexbench-desktop.
+// One way to support this is:
+// const appPath = process.platform === 'darwin' ? 'flexbench-desktop' : 'Flexbench';
+// module.exports = { APP_NAME:appPath }
+module.exports = {APP_NAME: "Flexbench"}
